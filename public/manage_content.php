1356ed25c3873c43674faf7abceb55e23326dcd8
Yassir-17 <jaferaa1@gmail.com>
2021-12-03 23:04:00 +0200

    locate structure files to repo

---
+++ public/manage_content.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="static/css/bootstrap.min.css" >
+
+    <title>Admin page</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+
+    <script src="static/js/jquery-3.3.1.min.js" ></script>
+    <script src="static/js/bootstrap.min.js" ></script>
+
+  </body>
+</html>
